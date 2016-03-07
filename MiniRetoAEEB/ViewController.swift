8d21f86a07f5c762f034c3046fefd3ad1687c738
Adriana Escobar <adrianaescobar@gmail.com>
2016-03-07 10:42:15 -0600

    Initial Commit

---
+++ MiniRetoAEEB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MiniRetoAEEB
+//
+//  Created by Adriana Escobar Bravo on 07/03/16.
+//  Copyright © 2016 Adriana Escobar Bravo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MiniRetoAEEBTests/MiniRetoAEEBTests.swift
@@ -0,0 +1,36 @@
+//
+//  MiniRetoAEEBTests.swift
+//  MiniRetoAEEBTests
+//
+//  Created by Adriana Escobar Bravo on 07/03/16.
+//  Copyright © 2016 Adriana Escobar Bravo. All rights reserved.
+//
+
+import XCTest
+@testable import MiniRetoAEEB
+
+class MiniRetoAEEBTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
